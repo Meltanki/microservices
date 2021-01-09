6f10d05f93434103a042a42b77542d01887dfa1b
Meltanki <hbeddo@gmail.com>
2021-01-09 13:30:12 -0700

    initial commit

---
+++ main/main/producer.py
@@ -0,0 +1,11 @@
+import pika, json
+
+params = pika.URLParameters('amqps://yyfsqfau:trBS-e95nlE3UuOGvMvfk10W1_unGlIq@gull.rmq.cloudamqp.com/yyfsqfau')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
